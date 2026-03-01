a3e06891fc469489de7d0f45e61023c3a8ebb51d
MOHD ABRAR <y6o87@students.keele.ac.uk>
2026-03-01 07:32:53 +0000

    Added play again button to GUI version

--- gui_game.py
+++ gui_game.py
@@ -12,21 +12,30 @@ def check_guess():
     except ValueError:
         result_label.config(text="please enter a valid number.")
 
+def play_again():
+    global secret
+    secret = random.randint(1, 10)
+    entry.delete(0, tk.END)
+    result_label.config(text="New game started! Guess again.")
+
 secret = random.randint(1, 10)
 
 window = tk.Tk()
-window.title("Guessing Game")
+window.title("Guessing Game - GUI Version")
 
 label = tk.Label(window, text="Guess a number between 1 and 10:")
-label.pack()
+label.pack(pady=5)
 
 entry = tk.Entry(window)
-entry.pack()
+entry.pack(pady=5)
 
 button = tk. Button(window, text="Submit Guess", command=check_guess)
-button.pack()
+button.pack(pady=5)
+
+play_again_button = tk.Button(window,text="Play Again", command=play_again)
+play_again_button.pack(pady=5)
 
 result_label = tk.Label(window, text="")
-result_label.pack()
+result_label.pack(pady=5)
 
 window.mainloop()
